a1902a89a2ed936455f4a8fe12cd89f63024ac09
slacker87 <racethesunlive@yahoo.com>
2015-04-07 16:15:28 +0000

    fix the urls for nasa api

--- client/client.js
+++ client/client.js
@@ -11,6 +11,6 @@ Template.hello.events({
   'click button': function () {
     // increment the counter when button is clicked
     Session.set('counter', Session.get('counter') + 1);
-    Meteor.call('NASApushSession');
+    Meteor.call('pushSession');
   }
 });
--- server/nasa_api.js
+++ server/nasa_api.js
@@ -1,31 +1,15 @@
 /** Methods to interact with nasa push lightstream server **/
 Meteor.methods({
-  /* Obtain the session token */
+  /* Open a session stream with nasa's server */
   pushSession : function() {
-    //this.unblock();
-    var url = "http://push1.jsc.nasa.gov/lightstreamer/create_session.txt";
-    var params = { params: 
-                    { 'LS_adapter_set': 'PROXYTELEMETRY',
-                      'LS_client_version': '5.0',
-                      'LS_domain': 'nasa.gov' }};
-    var headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-    HTTP.call("POST", url, params, headers, function(error, result){
-      if (error){
-        console.log(error);
-        //throw new Meteor.Error("season-error","There was an error obtaining the season token")
-      }else{
-        console.log(result);
-      }
-    });
-    // console.log(result.content);
-    // console.log(result.statusCode);
-    // console.log(result.data);
+    this.unblock();
+    return HTTP.call("http://push1.jsc.nasa.gov/lightstreamer/create_session.js?LS_domain=nasa.gov&LS_client_version=5.0&LS_adapter_set=PROXYTELEMETRY");
   },
-  /* Pull down data using aforementioned token */
+  /* modify what data we get back */
   sessionData : function() {
     //this.unblock();
-    var url = "http://push1.jsc.nasa.gov:80/lightstreamer/control.txt";
-    HTTP.call("POST", url, function(error, result){
+    var url = "http://push1.jsc.nasa.gov/lightstreamer/control.txt?LS_session=Sfa9420df22498cb5T0716411&LS_id=ISPWebItem&LS_schema=utc%20utcyear&LS_mode=RAW&LS_op=add&LS_table=1";
+    HTTP.call("GET", url, function(error, result){
       if (error){
         console.log(error);
         //throw new Meteor.Error("seasondata-error","There was an error obtaining the season data")
