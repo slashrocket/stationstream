fcac46ba669483c6d837bed66d811893eee9cd32
Daniel James McMahon <danieljmcmahon@gmail.com>
2015-04-09 13:09:29 -0500

    Remove button code and duplicate About link

--- client/client.js
+++ client/client.js
@@ -1,22 +1,6 @@
 // counter starts at 0
 Meteor.subscribe('isslocation');
 
-Session.setDefault('counter', 0);
-
-Template.home.helpers({
-  counter: function () {
-    return Session.get('counter');
-  }
-});
-
-Template.home.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
-    //Meteor.call('pushSession');
-  }
-});
-
 Template.lightstreamer.helpers({
     isslocations: function() {
       return isslocation.find();
