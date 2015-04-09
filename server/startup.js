5088da608289553513141c776980209721a150d3
Daniel Climent <stokp1@gmail.com>
2015-04-09 10:06:10 +0300

    started building the REST API

--- server/startup.js
+++ server/startup.js
@@ -1,3 +1,10 @@
 Meteor.startup(function () {
   Meteor.call("lightstreamerConnect")
+  Restivus.configure({
+    useAuth: true,
+    prettyJson: true
+  });
+  Restivus.addCollection(isslocation, {
+    excludedEndpoints: ['put', 'post', 'delete', 'deleteAll']
+  });
 });
