c60026e36d324db5a3f30760c1cfaf0729f5e4e5
Daniel Climent <stokp1@gmail.com>
2015-04-09 20:40:46 +0300

    refactor lightstreamer subscrition code

--- server/nasa_api.js
+++ server/nasa_api.js
@@ -9,57 +9,39 @@ Meteor.methods({
     var lightstreamer = Meteor.npmRequire('lightstreamer-client');
     var lsClient = new lightstreamer.LightstreamerClient("http://push.lightstreamer.com","ISSLIVE");  
     lsClient.connect();
-    var stationTelemetryx = new lightstreamer.Subscription("MERGE",["USLAB000032"], ["Value"]);
-    var stationTelemetryy = new lightstreamer.Subscription("MERGE",["USLAB000033"], ["Value"]);
-    var stationTelemetryz = new lightstreamer.Subscription("MERGE",["USLAB000034"], ["Value"]);
+    var stationTelemetry = new lightstreamer.Subscription("MERGE",["USLAB000032","USLAB000033","USLAB000034"], ["Value"]);
     lsClient.addListener({
       onStatusChange: function(newStatus) {
         console.log(newStatus);
       }
     });
-    lsClient.subscribe(stationTelemetryx);
-    lsClient.subscribe(stationTelemetryy);
-    lsClient.subscribe(stationTelemetryz);
-    stationTelemetryx.addListener({
+    lsClient.subscribe(stationTelemetry);
+    stationTelemetry.addListener({
       onSubscription: function() {
         console.log("SUBSCRIBED");
       },
       onUnsubscription: function() {
         console.log("UNSUBSCRIBED");
       },
-		  onItemUpdate: Meteor.bindEnvironment(function(update) {
-        var value = update.getValue("Value");
-        console.log(value)
-        isslocation.insert({type: 'positionx', position: value, time: Date.now()});
-		  	//$("#"+update.getItemName()).text(update.getValue("Value")); <-- was used to inject results into divs
-		  })
-    });
-    stationTelemetryy.addListener({
-      onSubscription: function() {
-        console.log("SUBSCRIBED");
-      },
-      onUnsubscription: function() {
-        console.log("UNSUBSCRIBED");
-      },
-		  onItemUpdate: Meteor.bindEnvironment(function(update) {
-        var value = update.getValue("Value");
-        isslocation.insert({type: 'positiony', position: value, time: Date.now()});
-		  	//$("#"+update.getItemName()).text(update.getValue("Value")); <-- was used to inject results into divs
-		  })
-    });
-    stationTelemetryz.addListener({
-      onSubscription: function() {
-        console.log("SUBSCRIBED");
-      },
-      onUnsubscription: function() {
-        console.log("UNSUBSCRIBED");
-      },
-		  onItemUpdate: Meteor.bindEnvironment(function(update) {
-        var value = update.getValue("Value");
-        isslocation.insert({type: 'positionz', position: value, time: Date.now()});
-		  	//$("#"+update.getItemName()).text(update.getValue("Value")); <-- was used to inject results into divs
-		  })
+      onItemUpdate: Meteor.bindEnvironment(function(update) {
+        switch (update.getItemName()){
+          case "USLAB000032":
+            var value = update.getValue("Value")
+            console.log("X: " + value);
+            isslocation.insert({type: 'positionx', position: value, time: Date.now()});
+            break;
+          case "USLAB000033":
+            var value = update.getValue("Value")
+            console.log("Y: " + value);
+            isslocation.insert({type: 'positiony', position: value, time: Date.now()});
+            break;
+          case "USLAB000034":
+            var value = update.getValue("Value")
+            console.log("Z: " + value);
+            isslocation.insert({type: 'positionz', position: value, time: Date.now()});
+            break;
+        } 
+      })
     });
   },
-  
 });
