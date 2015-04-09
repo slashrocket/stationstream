b56dcc63f0d301cc950c6578d7307df843874624
slacker87 <racethesunlive@yahoo.com>
2015-04-09 15:25:48 +0000

    fix sort and spelling

--- client/client.js
+++ client/client.js
@@ -21,13 +21,13 @@ Template.lightstreamer.helpers({
     isslocations: function() {
       return isslocation.find();
     },
-    iss_latest_x_possition: function() {
-      return isslocation.findOne({type: 'positionx'},{sort: {timestamp : 1}});
+    iss_latest_x_position: function() {
+      return isslocation.findOne({type: 'positionx'},{sort: {time : -1}});
     },
-    iss_latest_y_possition: function() {
-      return isslocation.findOne({type: 'positiony'},{sort: {timestamp : 1}});
+    iss_latest_y_position: function() {
+      return isslocation.findOne({type: 'positiony'},{sort: {time : -1}});
     },
-    iss_latest_z_possition: function() {
-      return isslocation.findOne({type: 'positionz'},{sort: {timestamp : 1}});
+    iss_latest_z_position: function() {
+      return isslocation.findOne({type: 'positionz'},{sort: {time : -1}});
     },
 });
