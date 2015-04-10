5bd2a00bf6b16890c238bc4346dbb9ad74a41e76
Daniel Climent <stokp1@gmail.com>
2015-04-10 18:07:05 +0300

    expire data for solar, air, water and computers

--- server/permissions.js
+++ server/permissions.js
@@ -4,3 +4,21 @@ isslocation.deny({
       doc.expires = new Date( new Date().getTime() + (36000000*24) );
   }
 });
+isssolar.deny({
+  insert: function(userId, doc) {
+      //Set expiry to 6 hours from now
+      doc.expires = new Date( new Date().getTime() + (36000000*6) );
+  }
+});
+issairwater.deny({
+  insert: function(userId, doc) {
+      //Set expiry to 7 days from now
+      doc.expires = new Date( new Date().getTime() + (36000000*24*7) );
+  }
+});
+isscomputer.deny({
+  insert: function(userId, doc) {
+      //Set expiry to 30 days from now
+      doc.expires = new Date( new Date().getTime() + (36000000*24*30) );
+  }
+});
