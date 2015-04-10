945bba83b14b42446941e4b6c73f119717dbfe0b
slacker87 <racethesunlive@yahoo.com>
2015-04-10 02:18:26 +0000

    add some more data to lightstreamer page

--- client/client.js
+++ client/client.js
@@ -1,5 +1,8 @@
 // counter starts at 0
 Meteor.subscribe('isslocation');
+Meteor.subscribe('issairwater');
+Meteor.subscribe('isscomputer');
+Meteor.subscribe('isssolar');
 
 Template.lightstreamer.helpers({
     isslocations: function() {
@@ -13,5 +16,35 @@ Template.lightstreamer.helpers({
     },
     iss_latest_z_position: function() {
       return isslocation.findOne({type: 'positionz'},{sort: {time : -1}});
+    },
+    iss_o2: function() {
+      return issairwater.findOne({type: 'o2'},{sort: {time : -1}});
+    },
+    iss_n2: function() {
+      return issairwater.findOne({type: 'n2'},{sort: {time : -1}});
+    },
+    iss_co2: function() {
+      return issairwater.findOne({type: 'co2'},{sort: {time : -1}});
+    },
+    iss_badh2o: function() {
+      return issairwater.findOne({type: 'badh2o'},{sort: {time : -1}});
+    },
+    iss_goodh2o: function() {
+      return issairwater.findOne({type: 'goodh2o'},{sort: {time : -1}});
+    },
+    iss_cabin_pressure: function() {
+      return issairwater.findOne({type: 'cabinpressure'},{sort: {time : -1}});
+    },
+    iss_cabin_temp: function() {
+      return issairwater.findOne({type: 'cabintemp'},{sort: {time : -1}});
+    },
+    iss_command_count: function() {
+      return isscomputer.findOne({type: 'commandcount'},{sort: {time : -1}});
+    },
+    iss_data_count: function() {
+      return isscomputer.findOne({type: 'datacount'},{sort: {time : -1}});
+    },
+    iss_pc_count: function() {
+      return isscomputer.findOne({type: 'pccount'},{sort: {time : -1}});
     },
 });
