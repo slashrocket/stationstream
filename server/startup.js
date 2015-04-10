370f817c0f879093557c98091c3bdc7207ebad25
Daniel Climent <stokp1@gmail.com>
2015-04-10 10:04:12 +0300

    Add latest data API endpoint

--- server/startup.js
+++ server/startup.js
@@ -1,10 +1,24 @@
 Meteor.startup(function () {
   Meteor.call("lightstreamerConnect")
   Restivus.configure({
-    useAuth: true,
+    useAuth: false,
     prettyJson: true
   });
-  Restivus.addCollection(isslocation, {
-    excludedEndpoints: ['put', 'post', 'delete', 'deleteAll']
+  Restivus.addRoute('isslocation/latest', {authRequired: false}, {
+    get: function(){
+      var positionx = isslocation.findOne({type: 'positionx'},{sort: {time : -1}});
+      var positiony = isslocation.findOne({type: 'positiony'},{sort: {time : -1}});
+      var positionz = isslocation.findOne({type: 'positionz'},{sort: {time : -1}});
+      var telemetry = {
+        telemetry: positionx.position,
+        positiony: positiony.position,
+        positionz: positionz.position
+      };
+      if (positionx && positiony && positionz){
+        return {status: 'success', data: telemetry};
+      }else{
+        return {statusCode: 404, body: {status: 'fail', message: 'Record not found.'}}
+      };
+    }
   });
 });
