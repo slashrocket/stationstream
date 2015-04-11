d410d23e2e03feaa7cd235d0d8ed1e39660ff05c
slacker87 <racethesunlive@yahoo.com>
2015-04-11 03:38:51 +0000

    graph all air and computer information

---
+++ client/compatibility/airwater.js
@@ -0,0 +1,57 @@
+function airwater(name, id, kind) {
+  var seriesData = []
+  var latest8 = issairwater.find({type: kind},{sort: {time : -1}, limit: 5});
+  latest8.forEach(function (item) {
+    seriesData.push([item.time, Number(item.value)]);
+  });
+
+  Highcharts.setOptions({
+    global: {
+      useUTC: false
+    }
+  });
+
+  $(id).highcharts({
+    chart: {
+      type: 'spline',
+      events: {
+        load: function () {
+          // set up the updating of the chart each second
+          var series = this.series[0];
+          setInterval(function () {
+            var last_airwater = series.data[series.data.length - 1];
+            var airwater = issairwater.findOne({type: kind},{sort: {time : -1}});
+            if ( Number(last_airwater.y) != Number(airwater.value) ) {
+              var x = airwater.time; // current time
+              var y = Number(airwater.value);
+              series.addPoint([x, y], true, true);
+            }
+          }, 2000);
+        }
+      }
+    },
+    title: {
+      text: name + ' - Live data'
+    },
+    xAxis: {
+      type: 'datetime',
+      tickAmount: 15
+    },
+    yAxis: {
+      title: {
+        type: 'logarithmic',
+        text: 'Value',
+        tickAmount: 15
+      },
+      plotLines: [{
+        value: 0,
+        width: 1,
+        color: '#808080'
+      }]
+    },
+    series: [{
+      name: kind,
+      data: seriesData
+    }]
+  });
+}
