a73353fbe76960d1988bf66af989d753561de928
Slacker87 <ccnp_pro@yahoo.com>
2015-05-01 17:58:12 +0000

    start adding a color pallete and enable scss

--- client/client.js
+++ client/client.js
@@ -1,35 +1,28 @@
 Meteor.subscribe('isslocation');
 Meteor.subscribe('isscomputer');
-           //Subscribe to Solar Data
-        Meteor.subscribe('1Avoltage_isssolar'),
-        Meteor.subscribe('1Bvoltage_isssolar'),
-        Meteor.subscribe('2Avoltage_isssolar'),
-        Meteor.subscribe('2Bvoltage_isssolar'),
-        Meteor.subscribe('3Avoltage_isssolar'),
-        Meteor.subscribe('3Bvoltage_isssolar'),
-        Meteor.subscribe('4Avoltage_isssolar'),
-        Meteor.subscribe('4BAvoltage_isssolar'),
-        // Subscribe to Air and Water Data
-        Meteor.subscribe('issairwater_o2'),
-        Meteor.subscribe('issairwater_n2'),
-        Meteor.subscribe('issairwater_co2'),
-        Meteor.subscribe('issairwater_badh2o'),
-        Meteor.subscribe('issairwater_goodh2o'),
-        Meteor.subscribe('issairwater_cabinpressure'),
-        Meteor.subscribe('issairwater_cabintemp'),
-        // Subscribe to Location Data
-        Meteor.subscribe('isslocation_x'),
-        Meteor.subscribe('isslocation_y'),
-        Meteor.subscribe('isslocation_z')
-//Meteor.subscribe('isssolar');
-//Meteor.subscribe('issairwater');
-//Meteor.subscribe('issairwater_n2');
-//Meteor.subscribe('issairwater_co2');
-//Meteor.subscribe('issairwater_o2');
-
-Template.cesium.onRendered(function() {
-  //var czml = czmlpacket();
-  //viewer.dataSources.add(Cesium.CzmlDataSource.load(czml));
+//Subscribe to Solar Data
+Meteor.subscribe('1Avoltage_isssolar'),
+Meteor.subscribe('1Bvoltage_isssolar'),
+Meteor.subscribe('2Avoltage_isssolar'),
+Meteor.subscribe('2Bvoltage_isssolar'),
+Meteor.subscribe('3Avoltage_isssolar'),
+Meteor.subscribe('3Bvoltage_isssolar'),
+Meteor.subscribe('4Avoltage_isssolar'),
+Meteor.subscribe('4BAvoltage_isssolar'),
+// Subscribe to Air and Water Data
+Meteor.subscribe('issairwater_o2'),
+Meteor.subscribe('issairwater_n2'),
+Meteor.subscribe('issairwater_co2'),
+Meteor.subscribe('issairwater_badh2o'),
+Meteor.subscribe('issairwater_goodh2o'),
+Meteor.subscribe('issairwater_cabinpressure'),
+Meteor.subscribe('issairwater_cabintemp'),
+// Subscribe to Location Data
+Meteor.subscribe('isslocation_x'),
+Meteor.subscribe('isslocation_y'),
+Meteor.subscribe('isslocation_z')
+        
+Template.home.onRendered(function() {
 });
 
 Template.analytics.onRendered(function () {
